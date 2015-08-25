631cdb90d150e2ab1b91874dd2f1bfa6682aec15
Jianhui Yuan <zuiwanyuan@gmail.com>
2015-08-25 15:45:51 +0800

    notify msg may read in batch

--- src/msg/async/Event.cc
+++ src/msg/async/Event.cc
@@ -46,10 +46,11 @@ class C_handle_notify : public EventCallback {
   C_handle_notify(EventCenter *c): center(c) {}
   void do_request(int fd_or_id) {
     char c[256];
+    int r = 0;
     do {
       center->already_wakeup.set(0);
-      read(fd_or_id, c, sizeof(c));
-    } while (center->already_wakeup.read());
+      r = read(fd_or_id, c, sizeof(c));
+    } while (center->already_wakeup.read() || r == sizeof(c));
   }
 };
 
